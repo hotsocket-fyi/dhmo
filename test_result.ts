7d317236784b35415162fc021b6127f873aff393
hotsocket <git@hotsocket.fyi>
2025-10-23 15:23:38 -0500

    whoops i need to licenseify that

--- test_result.ts
+++ test_result.ts
@@ -1,3 +1,9 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+
 import { Err, Ok, Result } from "./mod.ts";
 import { assert, assertEquals } from "@std/assert";
 
